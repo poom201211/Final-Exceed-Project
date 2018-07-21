3bbeea10fcd020d550161bde446c2b1a08369c9f
ThanaphonKeawjam <tongasura001@gmail.com>
2018-07-22 05:29:14 +0700

    update software

--- Software/Owner Folder/OwnerScript.js
+++ Software/Owner Folder/OwnerScript.js
@@ -2,13 +2,16 @@
 let type1 = "problem"
 let type2 = "level"
 let type3 = "room"
-let type4 = "check_empty"
+let type4 = "sos_room"
+let type5 = "sos_level"
 
 //data
-let problem = ''
-let level = ''
-let room = ''
-let check_empty = ''
+let problem = 0
+let level = 0
+let room = 0
+let check_empty = 0
+let sosRoom = 0
+let sosLevel = 0
 
 let getWeb = (type) => {
     $.ajax({
@@ -16,36 +19,53 @@ let getWeb = (type) => {
         url: `http://ecourse.cpe.ku.ac.th/exceed/api/buapalm-${type}/view`,
         dataType: "text",
         success: function (response) {
-            console.log(response + type)
+           // console.log(response + type)
             if (type === 'problem') {
                 if (response === '1') {
-                   problem = 'tissue'
+                    problem = 'tissue'
                 } else if (response === '2') {
-                    problem = 'clog'
+                    problem = clog
                 } else if (response === '3') {
                     problem = 'light'
                 }
             } else if (type === 'level') {
                 if (response === '1') {
-                    level = '1'
+                    level = 1
                 } else if (response === '2') {
-                    level = '2'
+                    level = 2
                 }
             } else if (type === 'room') {
                 if (response === '1') {
-                    room = '1'
+                    room = 1
                 } else if (response === '2') {
-                    room = '2'
+                    room = 2
                 } else if (response === '3') {
-                    room = '3'
+                    room = 3
                 } else if (response === '4') {
-                    room = '4'
+                    room = 4
                 }
-            } else if (type === 'check_empty') {
-                if (response === '0') {
-                    check_empty = 'no'
-                } else if (response === '1') {
-                    check_empty = 'yes'
+            } else if (type === 'sos_room') {
+                if(response === '0') {
+                    sosRoom = 0
+                }
+                if (response === '1') {
+                    sosRoom = 1
+                    callSOS()
+                } 
+                if (response === '2') {
+                    sosRoom = 2
+                }
+                if (response === '3') {
+                    sosRoom = 3
+                }
+                if (response === '4') {
+                    sosRoom = 4
+                }
+            } else if (type === 'sos_level') {
+                if (response === '1') {
+                    sosLevel = 1
+                } else if (response === '2') {
+                    sosLevel = 2
                 }
             }
 
@@ -53,8 +73,8 @@ let getWeb = (type) => {
         fail: function (response) {
             console.log(response)
         }
-        
     });
+
 }
 
 let getAll = () => {
@@ -62,65 +82,66 @@ let getAll = () => {
     getWeb(type2)
     getWeb(type3)
     getWeb(type4)
+    getWeb(type5)
+
+
+    sendTo()
+}
+
+let callSOS = () => {
+    $('#SOS').html(`<div style="background-color: red">level: ${sosLevel} room: ${sosRoom}</div>`)
 }
 
 let sendTo = () => {
-    console.log("here")
-    if (problem === 'tissue'){
-        $('#row1').append(`<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    if (problem === 'tissue') {
+        $(`#1row${level}_${room}`).html(`<div class="alert alert-warning alert-dismissible fade show" role="alert">
         <h4>level: ${level} room: ${room}</h4>
-        <button type="button" id="close-button" class="close" data-dismiss="alert" aria-label="Close">
+        <button type="button" id="close-button1" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
         </button>
         </div>`)
-    }else if (problem === 'clog'){
-        $('#row2').append(`<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    } else if (problem === 'clog') {
+        $(`#2row${level}_${room}`).html(`<div class="alert alert-warning alert-dismissible fade show" role="alert">
         <h4>level: ${level} room: ${room}</h4>
-        <button type="button" id="close-button" class="close" data-dismiss="alert" aria-label="Close">
+        <button type="button" id="close-button2" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
         </button>
         </div>`)
-    }else if (problem === 'light'){
-        $('#row3').append(`<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    } else if (problem === 'light') {
+        $(`#3row${level}_${room}`).html(`<div class="alert alert-warning alert-dismissible fade show" role="alert">
         <h4>level: ${level} room: ${room}</h4>
-        <button type="button" id="close-button" class="close" data-dismiss="alert" aria-label="Close">
+        <button type="button" id="close-button3" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
         </button>
         </div>`)
     }
+
 }
 
-let setWeb = () => {
-    let stage = -1
-    $('#close-button').on('click', function(){
-        $('#row1').hover(function(){
-            stage = 0
-        })
-        $('#row2').hover(function(){
-            stage = 0
-        })
-        $('#row3').hover(function(){
-            stage = 0
-        })
+let setup = () => {
+    $('#sos-button').on('click', function () {
+        $('#SOS').html(`<div>No Signal</div>`)
+        $.ajax({
+            type: "POST",
+            url: "http://ecourse.cpe.ku.ac.th/exceed/api/buapalm-sos_room/set?value=",
+            data: {
+                value: 0
+            },
+            dataType: "json",
+            success: function (response) {
+                
+            }
+        });
     })
 
+    sosRoom = 0
 
-    $.ajax({
-        type: "POST",
-        url: "http://ecourse.cpe.ku.ac.th/exceed/api/buapalm-problem/set",
-        data: {
-            value: stage
-        },
-        dataType: "json",
-        success: function (response) {
-            
-        }
-    });
+    $('#')
 }
 
 let init = () => {
+    setup()
     setInterval(getAll, 5000)
- //   setWeb()
 }
 
 $(init)
