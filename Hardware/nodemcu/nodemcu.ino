62f32277fed2963391e07ce33e461b96e794ea78
poonsht <poon_arsene_lupin@hotmail.com>
2018-07-21 21:26:00 +0700

    add chart-page

--- Hardware/nodemcu/nodemcu.ino
+++ Hardware/nodemcu/nodemcu.ino
@@ -10,20 +10,24 @@ struct ProjectData {
     uint32_t problem;
     uint32_t level;
     uint32_t room;
+    uint32_t sos_level;
+    uint32_t sos_room;
     // unit32_t timing;
     // uint32_t check_empty;
 
     
-} project_data = {0,2,1};
+} project_data = {0,2,1,0,0};
 
 struct ServerData {
     uint32_t problem;
     uint32_t level;
     uint32_t room;
+    uint32_t sos_level;
+    uint32_t sos_room;
     // unit32_t timing;
     // uint32_t check_empty;
    
-} server_data = {0,2,1};
+} server_data = {0,2,1,0,0};
 String const url = "http://ecourse.cpe.ku.ac.th/exceed/api/";
 const char GET_SERVER_DATA = 1;
 const char GET_SERVER_DATA_RESULT = 2;
@@ -200,7 +204,9 @@ void loop() {
     // uint32_t problem;
     // uint32_t level;
     // uint32_t room;
-    // unit32_t timing;
+    // unit32_t sos_level;
+    // uint32_t sos_room;
+    // uint32_t timing;
     // uint32_t check_empty;
 
         // server_data.problem = getAndPrint("problem");
@@ -212,13 +218,13 @@ void loop() {
         Serial.print("GET << problem : ");
         Serial.println(server_data.problem);
 
-        GET(get_builder("buapalm-level").c_str(), get_request,server_data.level);
-        Serial.print("GET << level : ");
-        Serial.println(server_data.level);
+        // GET(get_builder("buapalm-level").c_str(), get_request,server_data.level);
+        // Serial.print("GET << level : ");
+        // Serial.println(server_data.level);
 
-        GET(get_builder("buapalm-room").c_str(), get_request,server_data.room);
-        Serial.print("GET << room : ");
-        Serial.println(server_data.room);
+        // GET(get_builder("buapalm-room").c_str(), get_request,server_data.room);
+        // Serial.print("GET << room : ");
+        // Serial.println(server_data.room);
 
         // GET(get_builder("buapalm-check_empty").c_str(), get_request,server_data.check_empty);
         // Serial.print("GET << check_empty : ");
@@ -277,6 +283,15 @@ void loop() {
                     Serial.print("POST >> room : ");
                     Serial.println(room);
                     POST(set_builder("buapalm-room", room).c_str(), update_data_to_server_callback); 
+
+                    Serial.print("POST >> sos_level : ");
+                    Serial.println(sos_level);
+                    POST(set_builder("buapalm-sos_level", sos_level).c_str(), update_data_to_server_callback); 
+                    
+                    Serial.print("POST >> sos_room : ");
+                    Serial.println(sos_room);
+                    POST(set_builder("buapalm-sos_room", sos_room).c_str(), update_data_to_server_callback); 
+
 
                     // Serial.print("POST >> check_empty : ");
                     // Serial.println(check_empty);
